62e3f9a0f2ee4ce97a9b917032fdf33d714f4a6f
Hariharan2223 <hariharan2445@gmial.com>
2024-07-11 11:36:39 +0530

    uploaded django project

---
+++ django_project/shop_env/shop_project/shop_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.home,name="Home"),
+    path('register',views.register,name="Register"),
+    path('login/',views.login_page,name="login"),
+    path('logout/',views.logout_page,name="logout"),
+    path('collections',views.collections,name="Collections"),
+    path('collection/<str:name>/', views.collectionview, name='collection'),
+    path('collection/<str:cname>/<str:pname>', views.product_details, name='product_details'),
+    path('addtocart/',views.add_to_cart,name="addtocart"),
+    path('cart/',views.cart_page,name="cart"),
+    path('remove_cart/<str:uid>',views.remove_cart,name="remove_cart"),
+    path('fav/',views.fav_page,name="fav"),
+    path('fav_view/',views.fav_view,name="fav_view"),
+    path('remove_fav/<str:fid>',views.remove_fav,name="remove_fav"),
+]
